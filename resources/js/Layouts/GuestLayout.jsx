da3c3902e5a3c63251ddcd35aaad987fabf0f160
MiguelFS <lflores.221ds15@istta.edu.pe>
2025-07-10 12:53:04 -0500

    cambioLogo

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,4 +1,4 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import ApplicationLogo from '@/Components/ApplicationLogo1';
 import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
