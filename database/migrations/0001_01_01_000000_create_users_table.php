aee55f8a523a9d94afb4014c3ad039e9ad7b06af
MiguelFS <lflores.221ds15@istta.edu.pe>
2025-07-16 12:13:31 -0500

    correccion PE

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->enum('rol', ['admin', 'tutor', 'psicologo'])->default('tutor');
-            $table->string('programa_estudios', 45)->nullable();
+            $table->string('programa_estudios', 10)->nullable();
             $table->timestamps();
         });
 
